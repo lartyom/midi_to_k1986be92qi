16642c487fc2667806a9e3f8d42ea75ec263fb63
Artyom Lazarev <43204713+lartyom@users.noreply.github.com>
2020-11-19 18:48:47 +0300

    Beep player
    Playing the frequencies of notes by their number

--- musicalСard/Form1.cs
+++ musicalСard/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,7 +21,84 @@ namespace musicalСard
             InitializeComponent();
         }
 
-   
+        double[] notes = new double[]
+        {
+    27.5000,
+    29.1352,
+  30.8677,
+ 32.7032,
+    34.6478,
+  36.7081,
+    38.8909,
+  41.2034,
+  43.6535,
+    46.2493,
+  48.9994,
+    51.9131,
+  55.0000,
+    58.2705,
+  61.7354,
+ 65.4064,
+    69.2957,
+  73.4162,
+    77.7817,
+  82.4069,
+  87.3071,
+    92.4986,
+  97.9989,
+    103.826,
+  110.000,
+    116.541,
+  123.471,
+  130.813,
+    138.591,
+ 146.832,
+    155.563,
+ 164.814,
+  174.614,
+    184.997,
+ 195.998,
+    207.652,
+  220.000,
+    233.082,
+  246.942,
+  261.626,
+    277.183,
+ 293.665,
+    311.127,
+  329.628,
+  349.228,
+    369.994,
+  391.995,
+    415.305,
+  440.000,
+    466.164,
+  493.883,
+  523.251,
+    554.365,
+  587.330,
+    622.254,
+  659.255,
+  698.456,
+    739.989,
+  783.991,
+    830.609,
+  880.000,
+    932.328,
+  987.767,
+  1046.50,
+    1108.73,
+ 1174.66,
+    1244.51,
+  1318.51,
+  1396.91,
+    1479.98,
+  1567.98,
+    1661.22,
+  1760.00,
+    1864.66,
+  1975.53
+        };
 
         // Назначение: Хранить параметры заголовка MIDI файла. 
         // Применение: Структура создается при первом чтении MIDI файла.
@@ -230,6 +307,8 @@ namespace musicalСard
                 {
                     channelNote.noteInChannelNote DataD = (channelNote.noteInChannelNote)buffer.arrayNoteChannel[loop1];
                     richTextBox1.Text += DataD.roomNotes.ToString() + "," + DataD.noteTime.ToString();
+                    Console.Beep((int)Math.Round(notes[DataD.roomNotes-21]), Convert.ToInt32(DataD.noteTime));
+                    
                     if (loop1 != (buffer.arrayNoteChannel.Count - 1)) richTextBox1.Text += ", \t";
                 }
                 richTextBox1.Text += "};\n\n";
@@ -372,3 +451,4 @@ namespace musicalСard
             }
     }
 }
+
